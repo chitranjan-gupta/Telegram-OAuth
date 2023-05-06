b2ebf89dbc9d8da975775c953ce54ccfef7ee482
Chitranjan Gupta <37315561+chitranjan01@users.noreply.github.com>
2023-05-06 20:52:34 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -8,8 +8,10 @@ export default function Home() {
   const [message,setMessage] = useState("");
   useEffect(()=>{
     let mes = prompt("Enter the password");
-    console.log(mes);
     if(mes == "03122021"){
+      if(window.document){
+        window.document.body.style.backgroundImage = `url("./IMG-20220414-WA0009.jpg")`;
+      }
       msg.current.innerText = "Hii baccha \n all the best for tomorrow, tomorrow is yours day. We have trust on you that you will surely crack the exam with a good rank. Sorry for my bad behaviour that i have done in recent days, but trust me whatever i have done there is reason behind it. \n Love You baccha ❤️♾️ \n Best wishes from yours betuu ❤️.";
     }else{
       msg.current.innerText = "Failed";
