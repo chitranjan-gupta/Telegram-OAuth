5764a42accbff930d116b4832719a75742d4a593
Chitranjan Gupta <37315561+chitranjan01@users.noreply.github.com>
2023-05-06 20:16:24 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 import { useState, useRef, useEffect } from "react";
 import Image from 'next/image'
 import styles from './page.module.css'
-import shivam from "./IMG-20220414-WA0009.jpg"
+
 export default function Home() {
   const msg = useRef(null);
   const [message,setMessage] = useState("");
@@ -18,17 +18,6 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div><h1 id="msg" ref={msg}></h1></div>
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src={shivam}
-          alt="Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
     </main>
   )
 }
